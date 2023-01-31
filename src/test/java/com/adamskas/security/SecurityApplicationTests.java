d3bd0a126908e264033b93e97c538ea3058ef350
Adam Skaskiewicz <adam.skaskiewicz@envelo.pl>
2023-01-31 19:55:06 +0100

    feat: initial commit

---
+++ src/test/java/com/adamskas/security/SecurityApplicationTests.java
@@ -0,0 +1,13 @@
+package com.adamskas.security;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SecurityApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
